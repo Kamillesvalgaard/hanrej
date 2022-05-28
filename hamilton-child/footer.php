07095e736c13c22ab218122aaf240f1fd54776f0
Kamillesvalgaard <70881285+Kamillesvalgaard@users.noreply.github.com>
2022-05-28 08:31:03 +0200

    Add files via upload

---
+++ hamilton-child/footer.php
@@ -0,0 +1,71 @@
+		</main>
+		
+		<footer class="site-footer section-inner">
+
+
+<div id="footer">
+
+
+	<div id="email">
+
+
+
+
+	<p>
+	<h3 id="apart">BE A PART OF THE COMMUNITY 	</h3>
+	<p style="color: #ffffff">Sign up to our newsletter and recieve 10% off your first order</p>
+
+
+
+		<input id="input" type="email" name="EMAIL" placeholder="PUT YOUR E-MAIL HERE" required />
+	
+
+
+</p>
+<p>
+    <label>
+        <input  name="AGREE_TO_TERMS" type="checkbox" value="1" required=""> <a id="termsagree" href="https://kamillesvalgaard.com/kea/4sem/Hanrej/terms-conditions/"
+        target="_blank">I have read and agree to the terms &amp; conditions</a>
+    </label>
+</p>
+
+<form id="mc4wp-form-1" class="mc4wp-form mc4wp-form-591" method="post" data-id="591" data-name="Nyhedsbrev sign-up" ><div id="email" class="mc4wp-form-fields">
+
+<p>
+	<input id="signup" class="flex" id="submit" type="submit" value="SIGN UP" />
+</p><label style="display: none !important;">Leave this field empty if you&#8217;re human: <input type="text" name="_mc4wp_honeypot" value="" tabindex="-1" autocomplete="off" /></label><input type="hidden" name="_mc4wp_timestamp" value="1638874413" /><input type="hidden" name="_mc4wp_form_id" value="591" /><input type="hidden" name="_mc4wp_form_element_id" value="mc4wp-form-1" /><div class="mc4wp-response"></div></form>
+			
+
+	</div>
+	<div id="social">
+		<a target="blank" href="https://www.instagram.com/hanrej/">
+			<img src="https://kamillesvalgaard.com/kea/4sem/Hanrej/wp-content/themes/hamilton-child/IG-ikon.png" alt="insta-ikon">
+		</a>
+		<a target="blank" href="https://www.facebook.com/hanrejofficial/">
+			<img src="https://kamillesvalgaard.com/kea/4sem/Hanrej/wp-content/themes/hamilton-child/FB-ikon.png" alt="fb-ikon">
+		</a>
+	</div>
+
+	<div id="footerlinks">
+		<a href="https://kamillesvalgaard.com/kea/4sem/Hanrej/about/">ABOUT</a>
+		<a href="https://kamillesvalgaard.com/kea/4sem/Hanrej/contact/">CONTACT</a>
+		<a href="https://kamillesvalgaard.com/kea/4sem/Hanrej/retailers/">RETAILERS</a>
+		<a href="https://kamillesvalgaard.com/kea/4sem/Hanrej/care-information/">CARE INFORMATION</a>
+		<a href="https://kamillesvalgaard.com/kea/4sem/Hanrej/refund_returns/">REFUND & RETURNS POLICY</a>
+		<a href="https://kamillesvalgaard.com/kea/4sem/Hanrej/privatlivspolitik/">PRIVACY POLICY</a>
+		<a href="https://kamillesvalgaard.com/kea/4sem/Hanrej/terms-conditions/">TERMS & CONDITIONS</a>
+		<img id="footerlogo" src="https://kamillesvalgaard.com/kea/4sem/Hanrej/wp-content/themes/hamilton-child/logo_hvid.png" alt="logo">
+	<a href="https://dk.trustpilot.com/review/www.hanrej.com" target="blank">
+		<img id="trust" src="https://kamillesvalgaard.com/kea/4sem/Hanrej/wp-content/themes/hamilton-child/trustpilot.png" alt="trustpilot">
+	</a>
+
+	</div>
+</div>
+
+
+		</footer><!-- footer -->
+	    
+	    <?php wp_footer(); ?>
+	        
+	</body>
+</html>
